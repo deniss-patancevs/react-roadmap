25bd02e2ff9232a4154a2b1f444c465d1e5afd4a
Deniss Patancevs <deniss.patancevs@accenture.com>
2026-08-06 18:31:02 +0300

    fix: keep footer at the bottom of the page
    - implement sticky footer layout
    - make the main content fill the available viewport height
    - prevent footer from shifting during page loading

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,18 +1,30 @@
 import { Outlet } from "react-router";
+import styled from "styled-components";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer/";
 
+const Layout = styled.div`
+  min-height: 100vh;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const Main = styled.main`
+  flex: 1;
+`;
+
 function MainLayout() {
   return (
-    <div>
+    <Layout>
       <Header />
 
-      <main>
+      <Main>
         <Outlet />
-      </main>
+      </Main>
 
       <Footer />
-    </div>
+    </Layout>
   );
 }
 
